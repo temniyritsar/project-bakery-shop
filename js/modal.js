ecbaa1c914315f1251cea6bfd9e10a1d3342274f
temniyritsar <153980536+temniyritsar@users.noreply.github.com>
2024-01-05 15:48:54 +0200

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,12 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
 
-// const modalbtnOpen = document.querySelector('.modal-Btn-open');
-// const modalbtnClose = document.querySelector('.modal-Btn-close');
+const modalBtnOpen = document.querySelectorAll('.modal-btn-open');
+const modalBtnClose = document.querySelector('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
-modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnOpen.forEach((btn) => {
+    btn.addEventListener('click', toggleModal)
+})
 modalBtnClose.addEventListener('click', toggleModal);
 
-// modalbtnOpen.addEventListener('click', toggleModal);
-// modalbtnClose.addEventListener('click', toggleModal);
